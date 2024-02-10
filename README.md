17bac7ca76b97508c67d7d2cf8156a2aa6c0ae12
Joel Berglund <joelberglund4497@gmail.com>
2024-02-10 19:30:28 +0100

    waede

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# yatzi-dator
- 
+# Loggbok
+
+## vecka 6
+
+Denna veckan gjorde jag ganska mycket jag började med att fixa så man kunde bestämma hur många spelare som ska 
+kunna spela, Jag satte en gräns på fyra men igentligen är inte det pogrammets limmit utan jag skrev in att man 
+kunnde köra fyra för att göra det enkelt i början men jag ska lägga till så man kan välja ett eget alternativ. 
+jag fortsatte med att fixa så du kunnde sätta namn på varje spelare. allt detta ver änkelt att skapa. det 
+senaste jag gjorde var en random metod vilket var mycket svårare. Jag försökte först använda mig av **rand()** 
+funktionen, men den kunnde inte kombineras med en system time function så jag valde att använda **rng()** 
+vilket kan annvända sig av system time. Jag valde att hämta system time i millisekunder från början efter som 
+jag förvänta mig att den skulle vara snabb nog, men när jag skapade en lista med fem täningar i sig för alla 
+spelare fick jag samma "random" tal. jag satte därför en for loop som lopa typ en miljon gågner för att kika om 
+det var för kort tid vilket jag insåg var problemet. här kommer koden: 
+
+    array< int, 5 > RandomDice(int numbers_to_generate, string name){
+			array< int, 5 > random = {0, 0, 0, 0, 0};
+			hash<string> name_hash;
+			unsigned long int hash = name_hash(name);
+			hash %= 1000;
+			cout << hash << endl;
+			hash *= static_cast<int>(pow(hash, 2));
+			for (int i = 0; i < numbers_to_generate; i++){
+				auto system_time_nanoseconds = chrono::duration_cast<chrono::nanoseconds>(
+        			chrono::system_clock::now().time_since_epoch()
+    			).count();
+				sleep(0.00001);
+				system_time_nanoseconds *= static_cast<int>(pow(hash, 2));
+				cout << "yes " << system_time_nanoseconds;
+				mt19937 rng(system_time_nanoseconds);
+				random[i] = 1 + (rng() % 6);
+			}
+			return random;
+		}
+
+Som du ser bytte jag till nanosekunder. **(Jag borde nämna att det var Alex ide att använda system time men 
+köra system time upphöjt till två)**
+
+### **Varför behövs system time?**
+
+det är eftersom en dator kan inte skapa random tal utan det är **pseudo random**. pseudo random betyder att det 
+inte är random men ser random ut. allt detta gör att man måste bestämma ett **seed** som bestämmer vart ifrån 
+datorn du ska ta dina pseudo random tal från. Men använder jag bara mig av ststem time kan det bli ganska lätt 
+att förutspå vilket tal som kommer de förra så jag valde att implementera en hash av nammnet av spelaren. utan 
+att förklara varje rad kod så gör jag lite matte för att sen gångra nanosekunderna med den om vandlade hashen 
+upphöjt till två. 
+
+### **Om sleep()**
+
+sist tänkte jag skriva lite snabbt om mitt val att implementera en **sleep()**. Det är eftersom jag pratade med 
+Ronnie och han sa att när man skriver low level programerings språk så är det viktigt att ge rästen av datorns 
+system att lite tid på den kärnan som ditt pogramm körs på. Men det är inte ända anledningen jag gav speler en 
+sleep utan jag ville också garantera att ovasett hur snabb eller långsam din dator är ska systemtime hinna 
+ändras så inte samma det kan bli samma seed.
+
+## vecka 5
+
+Jag har startat projectet och det finns inte mycket och säga. Det finns inget som var speciellt svårt jag har suttit och tittat på syntaxen effter som det var runt ett år sen jag skrev något i c++.
+
--- main.cpp
+++ main.cpp
@@ -1,154 +1,147 @@
-#include <iostream>
-#include <cstdlib>
 #include <algorithm>
-#include <string>
-#include <chrono>
-#include <random>
-#include <unistd.h>
-#include <cmath>
-#include <functional>
 #include <bitset>
-using std::string;
-using std::cout;
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <random>
+#include <string>
+#include <unistd.h>
+using std::array;
 using std::cin;
-using std::vector;
+using std::cout;
 using std::endl;
 using std::hash;
-using std::array;
+using std::string;
+using std::vector;
 
 class Player {
-	private:
-		string name = "Player";
-		string combination_left[15] = {
-			"Ettor", "Tvåor", "Treor", "Fyror", "Femmor", "Sexor", 
-			"Ett par", "Två par", "Triss", "Fyrtal", "Kåk", "Liten stege", "Stor stege", 
-			"chans", "Yatzy"
-		};
-		int score = 0;
-		array< int, 5 > dice = {
-			0, 0, 0, 0, 0
-		};
-	public:
-		array< int, 5> GetDiceArray (){
-			return dice;
-		}				
-};
+private:
+  string name = "Player";
+  string combination_left[15] = {
+      "Ettor", "Tvåor",       "Treor",      "Fyror", "Femmor",
+      "Sexor", "Ett par",     "Två par",    "Triss", "Fyrtal",
+      "Kåk",   "Liten stege", "Stor stege", "chans", "Yatzy"};
+  int score = 0;
+  array<int, 5> dice = {0, 0, 0, 0, 0};
 
+public:
+  array<int, 5> GetDiceArray() { return dice; }
+
+  string GetPlayerName() { return name; }
+};
 
 class Board {
-	private:
-		bool playing = false;
-		vector< Player > players;
-	public:
-		void StartGame (){
-			bool if_start_game = true;
-			string ready = "n";
-			while (if_start_game){
-				cout << "Redo att spela yatzy (j/n): ";
-				cin >> ready;
-				for (int i = 0; i < ready.length(); i++){
-        			ready[i] = tolower(ready[i]);
-				}
-				if (ready == "n" or ready == "nej"){
-					if_start_game = false;
-				}
-				else if (ready == "j" or ready == "ja"){
-					playing = true;
-					if_start_game = false;
-				}
-			}
-		}
+private:
+  bool playing = false;
+  vector<Player> players;
 
-		string AddPlayerName (int index){
-			string name = "Player";
-			cout << "spelare " << index + 1 << " name: "; 
-    		cin >> name;
+public:
+  void StartGame() {
+    bool if_start_game = true;
+    string ready = "n";
+    while (if_start_game) {
+      cout << "Redo att spela yatzy (j/n): ";
+      cin >> ready;
+      for (int i = 0; i < ready.length(); i++) {
+        ready[i] = tolower(ready[i]);
+      }
+      if (ready == "n" or ready == "nej") {
+        if_start_game = false;
+      } else if (ready == "j" or ready == "ja") {
+        playing = true;
+        if_start_game = false;
+      }
+    }
+  }
 
-			return name;
-		}
+  string AddPlayerName(int index) {
+    string name = "Player";
+    cout << "spelare " << index + 1 << " name: ";
+    cin >> name;
 
-		void AddPlayerCount	(){
-			cout << "Hur många ska spela: " << endl 
-			<< "1. En spelare. Du kommer möta datorn" << endl
-			<< "2. Två spelare" << endl
-			<< "3. Tre spelare" << endl
-			<< "4. Fyra spelare ";
-			int player_count = 0;
-			cin >> player_count;
-			player_count = HandleUserInput(player_count);
+    return name;
+  }
 
-			for (int i = 0; i < player_count; ++i)
-    		{
-    		    players.push_back( Player{} );
-				AddPlayerName(i);
-			}
-		}
+  void AddPlayerCount() {
+    cout << "Hur många ska spela: " << endl
+         << "1. En spelare. Du kommer möta datorn" << endl
+         << "2. Två spelare" << endl
+         << "3. Tre spelare" << endl
+         << "4. Fyra spelare ";
+    int player_count = 0;
+    cin >> player_count;
+    player_count = HandleUserInput(player_count);
 
-		bool GetPlaying (){ return playing; }
+    for (int i = 0; i < player_count; ++i) {
+      players.push_back(Player{});
+      AddPlayerName(i);
+    }
+  }
 
-		int HandleUserInput (int user_input){
-			bool valid_user_input = false;
-			while (!valid_user_input)
-			{
-				if(cin.fail()) 
-				{
-					cin.clear(); 
-					cin.ignore(); 
-					cout << "Please enter an Integer only.";
-					cin >> user_input;
-				}else {
-					valid_user_input = true;
-				}
-			}
-			return user_input;
-		}
+  bool GetPlaying() { return playing; }
 
-		void AddRandomDiceToDiceArray (){
-			for(int i = 0; i < players.size(); i++){
-				players[i].GetDiceArray() = RandomDice(5);
-			}
-		}
+  int HandleUserInput(int user_input) {
+    bool valid_user_input = false;
+    while (!valid_user_input) {
+      if (cin.fail()) {
+        cin.clear();
+        cin.ignore();
+        cout << "Please enter an Integer only.";
+        cin >> user_input;
+      } else {
+        valid_user_input = true;
+      }
+    }
+    return user_input;
+  }
 
-		array< int, 5 > RandomDice(int numbers_to_generate){
-			array< int, 5 > random = {0, 0, 0, 0, 0};
-			sleep(0.00001);
-			auto system_time_nanoseconds = std::chrono::duration_cast<std::chrono::nanoseconds>(
-        		std::chrono::system_clock::now().time_since_epoch()
-    		).count();
-			std::mt19937 rng(system_time_nanoseconds);
-			system_time_nanoseconds *= static_cast<int>(pow(system_time_nanoseconds, 2));
-			for (int i = 0; i < numbers_to_generate; i++){				
-				random[i] = 1 + (rng() % 6);
-			}
-			return random;
-		}
+  void AddRandomDiceToDiceArray() {
+    for (int i = 0; i < players.size(); i++) {
+      players[i].GetDiceArray() = RandomDice(5);
+    }
+  }
 
-		//void PlayerTurn (){
-	//		for (int i = 0; i < players.size(); i++){
-	//			cout << players[i] << " tur" << endl;
-	//		}
-		//}
+  array<int, 5> RandomDice(int numbers_to_generate) {
+    array<int, 5> random = {0, 0, 0, 0, 0};
+    sleep(0.00001);
+    auto system_time_nanoseconds =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(
+            std::chrono::system_clock::now().time_since_epoch())
+            .count();
+    std::mt19937 rng(system_time_nanoseconds);
+    system_time_nanoseconds *=
+        static_cast<int>(pow(system_time_nanoseconds, 2));
+    for (int i = 0; i < numbers_to_generate; i++) {
+      random[i] = 1 + (rng() % 6);
+    }
+    return random;
+  }
+
+  void PlayerTurn() {
+    for (int i = 0; i < players.size(); i++) {
+      cout << players[i].GetPlayerName() << " tur" << endl;
+    }
+  }
 };
 
+int main() {
+  Board board;
+  board.StartGame();
 
-int main(){
-	Board board;	
-	board.StartGame();
+  if (board.GetPlaying() == false) {
+    return 0;
+  }
 
-	if (board.GetPlaying() == false){
-		return 0;
-	}
+  board.AddPlayerCount();
 
-	board.AddPlayerCount();	
-	
-	
-	while (board.GetPlaying()){
-		
-		board.AddRandomDiceToDiceArray();
+  while (board.GetPlaying()) {
 
+    board.AddRandomDiceToDiceArray();
 
-		return 0;
-	}
+    return 0;
+  }
 
-	return 0;
+  return 0;
 }
