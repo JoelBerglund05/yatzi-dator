8aeb3eae19f2a1b05e2b7e1c8bc12a483ac58781
Joel Berglund <joelberglund4497@gmail.com>
2024-02-10 17:29:51 +0100

    erre

--- main.cpp
+++ main.cpp
@@ -8,49 +8,83 @@
 #include <cmath>
 #include <functional>
 #include <bitset>
-using namespace std;
-using namespace std::chrono;
+using std::string;
+using std::cout;
+using std::cin;
+using std::vector;
+using std::endl;
+using std::hash;
+using std::array;
 
 class Player {
 	private:
-
-	public:
 		string name = "Player";
-		string combination_left[15] = {"Ettor", "Tvåor", "Treor", "Fyror", "Femmor", "Sexor", 
-		"Ett par", "Två par", "Triss", "Fyrtal", "Kåk", "Liten stege", "Stor stege", 
-		"chans", "Yatzy"};
+		string combination_left[15] = {
+			"Ettor", "Tvåor", "Treor", "Fyror", "Femmor", "Sexor", 
+			"Ett par", "Två par", "Triss", "Fyrtal", "Kåk", "Liten stege", "Stor stege", 
+			"chans", "Yatzy"
+		};
 		int score = 0;
-		array< int, 5 > dice = {0, 0, 0, 0, 0};
-
-		
+		array< int, 5 > dice = {
+			0, 0, 0, 0, 0
+		};
+	public:
+		array< int, 5> GetDiceArray (){
+			return dice;
+		}				
 };
 
 
 class Board {
 	private:
-		
-	public:
-		int dice[5] = {0, 0, 0, 0, 0};
 		bool playing = false;
-		
-		bool StartGame (){
-			bool start_game = true;
+		vector< Player > players;
+	public:
+		void StartGame (){
+			bool if_start_game = true;
 			string ready = "n";
-			while (start_game){
+			while (if_start_game){
 				cout << "Redo att spela yatzy (j/n): ";
 				cin >> ready;
 				for (int i = 0; i < ready.length(); i++){
         			ready[i] = tolower(ready[i]);
 				}
 				if (ready == "n" or ready == "nej"){
-					return false;
+					if_start_game = false;
 				}
 				else if (ready == "j" or ready == "ja"){
-					return true;
+					playing = true;
+					if_start_game = false;
 				}
 			}
-			return false;
-		}	
+		}
+
+		string AddPlayerName (int index){
+			string name = "Player";
+			cout << "spelare " << index + 1 << " name: "; 
+    		cin >> name;
+
+			return name;
+		}
+
+		void AddPlayerCount	(){
+			cout << "Hur många ska spela: " << endl 
+			<< "1. En spelare. Du kommer möta datorn" << endl
+			<< "2. Två spelare" << endl
+			<< "3. Tre spelare" << endl
+			<< "4. Fyra spelare ";
+			int player_count = 0;
+			cin >> player_count;
+			player_count = HandleUserInput(player_count);
+
+			for (int i = 0; i < player_count; ++i)
+    		{
+    		    players.push_back( Player{} );
+				AddPlayerName(i);
+			}
+		}
+
+		bool GetPlaying (){ return playing; }
 
 		int HandleUserInput (int user_input){
 			bool valid_user_input = false;
@@ -69,65 +103,51 @@ class Board {
 			return user_input;
 		}
 
-		array< int, 5 > RandomDice(int numbers_to_generate, string name){
+		void AddRandomDiceToDiceArray (){
+			for(int i = 0; i < players.size(); i++){
+				players[i].GetDiceArray() = RandomDice(5);
+			}
+		}
+
+		array< int, 5 > RandomDice(int numbers_to_generate){
 			array< int, 5 > random = {0, 0, 0, 0, 0};
-			hash<string> name_hash;
-			unsigned long int hash = name_hash(name);
-			hash %= 1000;
-			cout << hash << endl;
-			hash *= static_cast<int>(pow(hash, 2));
-			for (int i = 0; i < numbers_to_generate; i++){
-				auto system_time_nanoseconds = chrono::duration_cast<chrono::nanoseconds>(
-        			chrono::system_clock::now().time_since_epoch()
-    			).count();
-				sleep(0.00001);
-				system_time_nanoseconds *= static_cast<int>(pow(hash, 2));
-				cout << "yes " << system_time_nanoseconds;
-				mt19937 rng(system_time_nanoseconds);
+			sleep(0.00001);
+			auto system_time_nanoseconds = std::chrono::duration_cast<std::chrono::nanoseconds>(
+        		std::chrono::system_clock::now().time_since_epoch()
+    		).count();
+			std::mt19937 rng(system_time_nanoseconds);
+			system_time_nanoseconds *= static_cast<int>(pow(system_time_nanoseconds, 2));
+			for (int i = 0; i < numbers_to_generate; i++){				
 				random[i] = 1 + (rng() % 6);
 			}
 			return random;
 		}
+
+		//void PlayerTurn (){
+	//		for (int i = 0; i < players.size(); i++){
+	//			cout << players[i] << " tur" << endl;
+	//		}
+		//}
 };
 
 
 int main(){
 	Board board;	
-	board.playing = board.StartGame();
+	board.StartGame();
 
-	if (board.playing == false){
+	if (board.GetPlaying() == false){
 		return 0;
 	}
 
-	cout << "Hur många ska spela: " << endl 
-	<< "1. En spelare. Du kommer möta datorn" << endl
-	<< "2. Två spelare" << endl
-	<< "3. Tre spelare" << endl
-	<< "4. Fyra spelare";
-	int user_input = 0;
-	cin >> user_input;
-
-	vector< Player > players;
-	string name = "Player";
-
-    for (int i = 0; i < user_input; ++i)
-    {
-		cout << "Pleas enert player " << i + 1<< " name: "; 
-        cin >> name;
-        players.push_back( Player{ name = name} );
-		cout << "Hej " << players[i].name << endl; 
-	}
-
-	while (board.playing){
-		for(int i = 0; i < players.size(); i++){
-			players[i].dice = board.RandomDice(5, players[i].name);
-			for (int index = 0; index < players[i].dice.size(); index++){
-				cout << players[i].dice[index] << " ";
-			}
-			cout << endl;
-		}
+	board.AddPlayerCount();	
+	
+	
+	while (board.GetPlaying()){
 		
-		board.playing = false;
+		board.AddRandomDiceToDiceArray();
+
+
+		return 0;
 	}
 
 	return 0;
